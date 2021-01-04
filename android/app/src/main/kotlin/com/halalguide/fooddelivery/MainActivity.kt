8bf3bedc4056341fb368b435cde87b910cbc2b2a
Flutter App <brijeshbodariya@gmail.com>
2021-01-04 14:05:26 +0500

    Android Config

--- android/app/src/main/kotlin/com/halalguide/fooddelivery/MainActivity.kt
+++ android/app/src/main/kotlin/com/halalguide/fooddelivery/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.halalguide.fooddelivery
 
-import android.os.Bundle
-import io.flutter.app.FlutterActivity;
-
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
- 
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
